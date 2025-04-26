0f72fa6ef431b7cfc92db66ad8d89e823f2f5efb
Xiaohan Zhong <yanyin180104@gmail.com>
2025-04-26 14:50:22 -0500

    fix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -168,8 +168,8 @@ public final class Constants {
     public static final double kIntakeSpeed = 0.04;    // Precise slow speed for sensor
     public static final double kIntakeSpeed1 = 0.15;
     public static final double kReverseSpeed = -0.2;
-    public static final double kL1Speed = 0.75;
-    public static final double kL1SpeedLow = 0.1;
+    public static final double kL1Speed = 0.5;
+    public static final double kL1SpeedLow = 0.2;
     public static final double kL24Speed = 0.65;
     public static final double kL24SpeedLow = 0.1;
     public static final double kIndexSpeed = 0.1;
